11e3171a175a5c3b197a2b54b9343735ad2e88f9
vohungvi <vohungvi27@gmail.com>
2020-04-09 18:12:52 +0700

    - make code easier to read

--- ConvertJpgToPng/WinMain.cpp
+++ ConvertJpgToPng/WinMain.cpp
@@ -13,11 +13,11 @@ void main(int argc, char** argv)
 	cv::Mat matGRBA;
 	cv::cvtColor(mat, matGRBA, CV_BGR2BGRA);
 
-	for (int y = 0; y < mat.rows; y++)
+	for (int y = 0; y < matGRBA.rows; y++)
 	{
-		for (int x = 0; x < mat.cols; x++)
+		for (int x = 0; x < matGRBA.cols; x++)
 		{
-			cv::Vec3b val = mat.at<cv::Vec3b>(y, x);
+			cv::Vec4b val = matGRBA.at<cv::Vec4b>(y, x);
 			if (val[0] > 200 && val[1] > 200 && val[2] > 200)
 			{
 				matGRBA.at<cv::Vec4b>(y, x) = cv::Vec4b(0, 0, 0, 0);
